c05835a2eab0e7ec6d5fd9fe726de87a2106c6b8
Lucas <lucas@aitc.com.tw>
2022-03-10 17:27:00 +0800

    First 上傳專案

---
+++ src/components/LeftRightJumping.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="jumpBorder">
+    <div :class="['leftDiv', { active: bbb }]">asdfasfss</div>
+    <div :class="['rightDiv', { active: bbb }]">afdsasfsfsdafs</div>
+  </div>
+  <button @click="aaa">出現吧!</button>
+</template>
+<script>
+import { ref } from "vue";
+export default {
+  name: "LeftRightJumping",
+  props: {},
+  setup() {
+    const bbb = ref(false);
+    const aaa = () => {
+      bbb.value = bbb.value == false ? true : false;
+      console.log(bbb.value);
+    };
+
+    return {
+      aaa,
+      bbb,
+    };
+  },
+};
+</script>
+<style lang="scss">
+@import "@/assets/scss/leftright.scss";
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <LeftRightJumping />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import LeftRightJumping from "@/components/LeftRightJumping.vue";
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    LeftRightJumping,
+  },
+};
+</script>
